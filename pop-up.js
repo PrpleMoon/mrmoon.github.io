8b00550192fd8c17637fa2f5ddaf074c3db8d5b9
PrpleMoon <106021109+PrpleMoon@users.noreply.github.com>
2023-10-09 18:37:27 +0200

    Add files via upload

---
+++ pop-up.js
@@ -0,0 +1,10 @@
+
+let button = document.getElementById('toggleButton');
+let count = 0;
+
+button.addEventListener('click', function() {
+    count++;
+    if (count % 2 === 1) {
+        alert('HAHA!YOU WISH!');
+    }
+});
