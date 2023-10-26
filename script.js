70ec174c9a37ea6100b4e4f331c028645261d9a0
PrpleMoon <thomassamuel@duck.com>
2023-10-26 19:51:38 +0200

    download

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const downloadButton = document.querySelector('.download-button');
-    const downloadLink = 'https://github.com/PrpleMoon/Purples-Pack/releases/download/download/Purples-Pack.exe'; // Replace with your actual download link
+    const downloadLink = 'https://github.com/PrpleMoon/modrinth/releases/download/v1.0.0/PurplesPack.exe'; // Replace with your actual download link
 
     downloadButton.addEventListener('click', function() {
         window.location.href = downloadLink;
