8928841daa9aabbadbbb6c242e28fc3865e7b130
PrpleMoon <thomassamuel@duck.com>
2023-12-24 11:34:04 +0100

    download

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const downloadButton = document.querySelector('.download-button');
-    const downloadLink = 'https://github.com/PrpleMoon/modrinth/releases/download/v1.0.0/Installer.bat'; // Replace with your actual download link
-
+    const secondButton = document.querySelector('.download-button-ita');
+    const downloadLink = 'https://github.com/PrpleMoon/modrinth/releases/download/v1.0.0/Install.bat';
+    const secondLink = 'https://github.com/PrpleMoon/modrinth/releases/download/v1.0.0/Installa.bat';
     downloadButton.addEventListener('click', function() {
         window.location.href = downloadLink;
+    });
+    secondButton.addEventListener('click', function() {
+        window.location.href = secondLink;
     });
 });
